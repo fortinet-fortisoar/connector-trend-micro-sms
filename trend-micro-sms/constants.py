66bec1a6293e0777a71b35daa30f735636cac16f
Naili Mahdi <39757299+ftnt-cse@users.noreply.github.com>
2022-02-07 19:41:26 +0400

    1.0.1a

---
+++ trend-micro-sms/constants.py
@@ -0,0 +1,11 @@
+PARAM_MAPPING = {
+    "IP": "ip",
+    "IPv4": "ipv4",
+    "IPv6": "ipv6",
+    "URL": "url",
+    "DNS": "dns",
+    "ALL": "all",
+    "USER": "user",
+    "REPDV": "repdv",
+    "ENTRY": "entry"
+}
